9f0581207daa5fffccf7b9fcd6285cf9fa0db29b
montero.villar.albert <montero.villar.albert@alumnat.copernic.cat>
2023-02-11 14:51:21 +0100

    Ejercicio 1 Hibernate.

---
+++ src/main/java/m06uf2ex1/M06UF2EX1.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package m06uf2ex1;
+
+import Modelo.Stock;
+import com.github.javafaker.Faker;
+import java.util.ArrayList;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.exception.ConstraintViolationException;
+
+/**
+ *
+ * @author Albert
+ */
+public class M06UF2EX1 {
+    
+    private static SessionFactory factory;
+    private static Session session;
+    private static final Logger logger = LogManager.getLogger(M06UF2EX1.class);
+    
+
+    public static void main(String[] args) {
+        
+        
+            try{
+            
+            factory = new Configuration().configure("hibernateConfig/hibernate.cfg.xml").buildSessionFactory();
+            
+            logger.trace("Iniciamos session");
+            session = factory.openSession();
+            
+            logger.trace("Iniciamos transaccion...");
+            session.getTransaction().begin();
+            
+            logger.trace("Creamos un ArrayList de Stocks");
+            
+            ArrayList<Stock> stocks = new ArrayList<Stock>();
+            
+            logger.trace("Creamos el objeto Faker");
+            
+            Faker faker = new Faker();
+
+            logger.trace("Añadimos 1000 objetos inventados al ArrayList");
+            
+            for(int i=0; i<1000; i++){
+                Stock stock = new Stock(faker.idNumber().valid(),faker.commerce().productName());
+                stocks.add(stock);
+            }
+            
+            logger.trace("Persistimos el estado del ArrayList de objetos");
+            
+            session.persist(stocks);
+            
+            logger.info("Finalitzem transacciÃ³ i desem a BBDD...");
+            
+            session.getTransaction().commit();
+            
+        
+        } catch (ConstraintViolationException ex){
+             if (session.getTransaction() !=null) 
+                 session.getTransaction().rollback();
+             logger.error("ViolaciÃ³ de la restricciÃ³: " + ex.getMessage());
+            
+        } catch (HibernateException ex) {
+            if (session.getTransaction()!=null) 
+                session.getTransaction().rollback();
+            
+            logger.error("ExcepciÃ³ d'hibernate: " + ex.getMessage());
+      } catch (Exception ex){
+             if (session.getTransaction() !=null) 
+                 session.getTransaction().rollback();
+             logger.error("ExcepciÃ³: " + ex.getMessage());
+            
+        }
+        finally {
+            
+         //Tanquem la sessiÃ³
+         session.close();
+         
+         //Finalitzem Hibernate
+         factory.close();
+      }
+    }
+}
